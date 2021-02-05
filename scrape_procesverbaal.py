4a1e0ebe049ff552a831027e6660413d8578ad6e
Floris Jansen <florismartijnjansen@gmail.com>
2021-02-05 23:03:44 +0100

    switch to PyMuPDF/fitz from tabula-py for parsing the 2021 list + speed

--- scrape_procesverbaal.py
+++ scrape_procesverbaal.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tabula
+import fitz
 import argparse
 import re
+import os
 import json
 import time
 
-# Variables
 
-names_re = "(?P<voorletters>([A-Z.]+)) \((?P<voornaam>.+)\) \((?P<geslacht>\w)\)"
-backup_names_re = "(?P<voorletters>([A-Z.]+)) \((?P<geslacht>\w)\)"
-kieskring_area = (40, 30, 75, 544)
-partij_area_kieskring = (90, 30, 110, 544)
-partij_area_normal = (70, 30, 76, 544)
-personen_area_kieskring = (100, 30, 800, 544)
-personen_area_partij = (80, 30, 800, 544)
-personen_area_normal = (70, 30, 800, 544)
+
+# Regexes
+
+voorletters_re = "(?P<voorletters>([A-Z.]+))"
+voornaam_re = "\((?P<voornaam>\w{2,})\)"
+geslacht_re = "\((?P<geslacht>\w)\)"
+
+
 
 # Passable variables
 
@@ -24,8 +24,6 @@ parser = argparse.ArgumentParser(
     description="Parse a procesverbaal PDF to a JSON file with all listed people")
 parser.add_argument("-f", "--file", action="store",
     help="The procesverbaal PDF with just the pages with lists")
-parser.add_argument("-e", "--endpage", action="store",
-    help="The last page number X for the parsed page range 1-X")
 
 args = parser.parse_args()
 
@@ -34,146 +32,209 @@ if args.file:
 else:
     file = "procesverbaal_content.pdf"
 
-if args.endpage:
-    file_end = int(args.endpage)
-else:
-    file_end = 681
 
-# Performance showing
-
-start_time = time.time()
 
 # Fillable variables
 
-kieskringen = {}
-partijen_pages = {}
-partijen = {}
+kieskringen = []
+partijen = []
 partijlijsten = {}
-pagina_lijsten = []
 geslachten = []
 
-laatste_kieskring = ''
-laatste_partij = ''
+# Status variables
+
+next_is_party = False
+next_is_last_name = False
+next_is_initials = False
+next_is_location = False
 
 print("🏎  start")
 
-# Run through all pages to find the kieskringen
-for page_number in range(1, file_end):
-    page_kind = 3 # Kieskring
-    # Try getting kieskring
-    kieskring_tab = tabula.read_pdf(file, pages=page_number, area=kieskring_area, guess=True) # Kieskring
-    # See if we have a kieskring
-    try:
-        kieskring_naam = kieskring_tab[0].columns[0]
-    except IndexError:
-        page_kind -= 1
 
-    if page_kind == 3:
-        # Kieskring page
-        laatste_kieskring = kieskring_naam
-        kieskringen[page_number] = laatste_kieskring
-        party_tab = tabula.read_pdf(file, pages=page_number, area=partij_area_kieskring, guess=True)
-        print("Working on {} (on page {}, after {}sec)".format(laatste_kieskring, page_number, int(time.time() - start_time)))
-    else:
-        # Other page: try getting party
-        party_tab = tabula.read_pdf(file, pages=page_number, area=partij_area_normal, guess=True)
 
-    # See if we have a party
-    try:
-        partij_naam = party_tab[0].columns[1]
-        if partij_naam == 'voorletters':
-            raise IndexError
-    except IndexError:
-        page_kind -= 1
+print("📁 creating output folder if it doesn’t exist")
 
-    if page_kind == 3:
-        laatste_partij = partij_naam
-        partijen_pages[page_number] = partij_naam
-        # check if party is already in collection; if not, add party
-        if laatste_partij not in partijen:
-            partijen[laatste_partij] = {}
-        personen_tab = tabula.read_pdf(file, pages=page_number, area=personen_area_kieskring, guess=True)
-        print("- {} (on page {}, after {}sec)".format(laatste_partij, page_number, int(time.time() - start_time)))
-    elif page_kind == 2:
-        laatste_partij = partij_naam
-        partijen_pages[page_number] = partij_naam
-        # check if party is already in collection; if not, add party
-        if laatste_partij not in partijen:
-            partijen[laatste_partij] = {}
-        personen_tab = tabula.read_pdf(file, pages=page_number, area=personen_area_partij, guess=True)
-        print("- {} (on page {}, after {}sec)".format(laatste_partij, page_number, int(time.time() - start_time)))
-    else:
-        personen_tab = tabula.read_pdf(file, pages=page_number, area=personen_area_normal, guess=True)
+if not os.path.exists('output'):
+    os.mkdir('output')
 
-    pagina_lijsten.append({
-        'kieskring': laatste_kieskring,
-        'partij': laatste_partij,
-        'personen': personen_tab[0]
-    })
-    # print('Page {} is kind {}'.format(page_number, page_kind))
+# Create file name for the plain text output
+output_txt = "{}.txt".format(file)
+
+# Open text file to store procesverbaal in plain text
+procesverbaal_txt = open(output_txt, 'w')
+
+# Open PDF
+with fitz.open(file) as doc:
+    # Flip through pages
+    for page in doc:
+        # Extracting text
+        text = page.getText()
+        # Store text in plain text file
+        procesverbaal_txt.write(text)
+
+# Close and save plain text procesverbaal
+procesverbaal_txt.close()
+
+print("💾 the procesverbaal is now plain text")
 
 
 
-print("👥 moving to creating people")
+# Open plain text procesverbaal again for processing
+procesverbaal_txt = open(output_txt, 'r')
+# Create a list of all the lines in the procesverbaal
+lines = procesverbaal_txt.read().splitlines()
 
-# Loop through pages
-for lijst in pagina_lijsten:
-    kieskring = lijst['kieskring']
-    partij = lijst['partij']
-    personen = lijst['personen']
 
-    amount = personen.index.stop
-    i = 0
-    # loop through personen on page
-    while i < amount:
-        # get array from tabula entry
-        persoon_array = personen.values[i]
 
-        name_string = persoon_array[2]
-        name_match = re.search(names_re, name_string)
+# Looping through all lines of procesverbaal
 
+for line in lines:
+
+    # Remove (trailing) spaces
+    line = line.strip()
+
+    # If, according to the previous line, this should be a party name
+    if next_is_party:
+        # Memorize it for later use
+        partij = line
+        # Store it
+        if partij not in partijen:
+            partijen.append(partij)
+            partijlijsten[partij] = {}
+        # Make sure not to treat the next line as the party name
+        next_is_party = False
+
+        continue
+
+    # If, according to the previous line, this should be a last name
+    if next_is_last_name:
+        # Memorize it for later use
+        achternaam = line
+        # Make sure to treat the next line as initials and such
+        next_is_initials = True
+        next_is_last_name = False
+
+        continue
+
+    # If it’s Dave Borghuis, who has a two-line last name
+    if line == "Borg":
+        # Fix his last name
+        achternaam += " Borg"
+
+        # Make sure to treat the next line as initials and such
+        next_is_initials = True
+        next_is_last_name = False
+
+        continue
+
+    # If, according to the previous line, this should be initials and such
+    if next_is_initials:
+
+        # Try if there’s initials
+        voorletter_match = re.search(voorletters_re, line)
         try:
-            full_name = '{} {}'.format(name_match.group('voornaam'), persoon_array[1])
-            voorletters = name_match.group('voorletters')
-            voornaam = name_match.group('voornaam')
-            geslacht = name_match.group('geslacht')
+            voorletters = voorletter_match.group('voorletters')
         except AttributeError:
-            backup_name_match = re.search(backup_names_re, name_string)
+            print("voorletters not found in {}".format(line))
 
-            try:
-                full_name = '{} {}'.format(name_match.group('voorletters'), persoon_array[1])
-                voorletters = name_match.group('voorletters')
-                geslacht = name_match.group('geslacht')
-            except AttributeError:
-                full_name = '{} {}'.format(persoon_array[2], persoon_array[1])
-                voorletters = persoon_array[2]
-                voornaam = ""
-                geslacht = ""
+        # Try if there’s a first name
+        voornaam_match = re.search(voornaam_re, line)
+        try:
+            voornaam = voornaam_match.group('voornaam')
+        except AttributeError:
+            voornaam = ""
 
+        # Try if there’s a sex
+        geslacht_match = re.search(geslacht_re, line)
+        try:
+            geslacht = geslacht_match.group('geslacht')
+        except AttributeError:
+            geslacht = ""
+
+        # Make sure we have list of all sexes to print
         if geslacht not in geslachten:
             geslachten.append(geslacht)
 
+        # Make sure to treat the next line as their location
+        next_is_initials = False
+        next_is_location = True
+
+        continue
+
+    # If, according to the previous line, this should be their location
+    if next_is_location:
+        # Memorize
+        woonplaats = line
+        # Store candidate in party
+        if voornaam:
+            full_name = "{} {}".format(voornaam, achternaam)
+        else:
+            full_name = "{} {}".format(voorletters, achternaam)
+
         # check if person is already in party
-        if full_name in partijen[partij]:
+        if full_name in partijlijsten[partij]:
             # if so, add kiesdistrict
-            partijen[partij][full_name]['kieskringen'].append(lijst['kieskring'])
+            partijlijsten[partij][full_name]['kieskringen'].append(kieskring)
         else:
             # if not,
             # - create person object
             person = {
-                'lijstnummer': persoon_array[0],
+                'lijstnummer': lijstnummer,
                 'kieskringen': [kieskring],
                 'naam': full_name,
-                'achternaam': persoon_array[1],
+                'achternaam': achternaam,
                 'voorletters': voorletters,
                 'voornaam': voornaam,
                 'geslacht': geslacht,
-                'stad': persoon_array[3],
+                'stad': woonplaats,
             }
             # - append person to party object
-            partijen[partij][full_name] = person
+            partijlijsten[partij][full_name] = person
 
-        i += 1
+        # Make sure not to treat the next line as the party name
+        next_is_location = False
+
+        continue
+
+    # If the line contains a lot of underscores, one of these strings or nothing
+    if "___" in line or \
+       "pagina" in line or \
+       "naam kandidaat" in line or \
+       "voorletters" in line or \
+       "woonplaats" in line or \
+       not line:
+        # it’s at the page break or table heading and we can ignore it
+        continue
+
+    # If the kieskring is announced
+    if "Kieskring" in line:
+        # Find just that name
+        kieskring_match = re.search("Kieskring (?P<kieskring>[A-Za-z-\']+)", line)
+        # Memorize it for later use
+        kieskring = kieskring_match.group('kieskring')
+        # Store it
+        kieskringen.append(kieskring)
+
+        continue
+
+    # If it’s the one line before a party name
+    if re.match("[A-Z][0-9]?$", line) != None:
+        # Make sure to treat the next line as the party name
+        next_is_party = True
+
+        continue
+
+    # If it’s a lijstnummer
+    if line.isdigit():
+        # Memorize it for later use
+        lijstnummer = int(line)
+        # Make sure to treat the next line as a last name
+        next_is_last_name = True
+
+        continue
+
+    print("{} is a problematic line in {}".format(line, partij))
+
 
 
 
@@ -183,7 +244,7 @@ partijen_json = {}
 
 for partij in partijen:
     # Safe name for files
-    illegal_chars = ['(', ')', '.', '/']
+    illegal_chars = ['(', ')', '.', '/', ',']
     safe_partij = partij
     for char in illegal_chars:
         safe_partij = safe_partij.replace(char, '')
@@ -196,13 +257,16 @@ for partij in partijen:
     }
 
     with open("output/{}".format(filename), 'w') as fp:
-        json.dump(partijen[partij], fp)
+        json.dump(partijlijsten[partij], fp)
 
 print("📄 storing parties in json files")
 
 with open("output/partijen.json", 'w') as fp:
     json.dump(partijen_json, fp)
 
+
 print("🏳️‍🌈  geslachten: {}".format(", ".join(geslachten)))
+print("🎉 partijen: {}".format(", ".join(partijen)))
+print("⭕️ kieskringen: {}".format(", ".join(kieskringen)))
 
 print("✅ done")
