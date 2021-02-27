fa2239bb9c34b9740f81ae83fa605a7715ca43a3
Floris Jansen <florismartijnjansen@gmail.com>
2021-02-27 19:35:40 +0100

    cleaning up file with pretty prints

--- kandidaten_add-gemeente-provincie.py
+++ kandidaten_add-gemeente-provincie.py
@@ -1,18 +1,36 @@
-import json
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-folder = "kandidaten_2021_kiesraad"
+import json
+import argparse
+
+
+
+# Passable variables
+
+parser.add_argument("-f", "--folder", action="store",
+    help="the folder with the party json files")
+
+
+if args.folder:
+    folder = args.folder
+else:
+    folder = "kandidaten_2021_kiesraad"
+
 kandidaten_file = "{}/kandidaten.json".format(folder)
 partijen_file = "{}/partijen.json".format(folder)
-with open(partijen_file, 'r') as pf:
-    partijen = json.load(pf)
+
+
+
+# Enriching function
 
 def add_info(kandidaten_dict):
     for kandi in kandidaten_dict:
-        print(kandidaten_dict[kandi])
+        # print(kandidaten_dict[kandi])
 
         # zoeken tussen alle woonplaatsen
         wnplts = woonplaats_data_dict.get(kandidaten_dict[kandi].get('stad'))
-        print("Woonplaats;", wnplts)
+        # print("Woonplaats;", wnplts)
 
         # als de woonplaats niet is gevonden, zoeken tussen alle gemeenten
         if wnplts is None:
@@ -29,11 +47,19 @@ def add_info(kandidaten_dict):
         kandidaten_dict[kandi]['provincie'] = wnplts['provincie']
     return kandidaten_dict
 
-# Importeer woonplaats_data json
+
+
+print("🌐 Reading location data")
+
+# Importeer woonplaats_data JSON
 with open('woonplaats_data.json') as json_file:
     woonplaats_data_dict = json.load(json_file)
 
-# Importeer kandidaten json
+
+
+print("👪 Working on candidates file")
+
+# Importeer kandidaten JSON
 with open(kandidaten_file, 'r') as fp:
     kandidaten_dict = json.load(fp)
 
@@ -43,12 +69,21 @@ kandidaten_dict = add_info(kandidaten_dict)
 with open(kandidaten_file, "w") as outfile:
     json.dump(kandidaten_dict, outfile)
 
+
+
+print("👥 Moving on to parties")
+
+# Import party list
+with open(partijen_file, 'r') as pf:
+    partijen = json.load(pf)
+
+# Cycle through parties
 for partij in partijen:
     print("- working on {}".format(partij))
 
     partij_file = "{}/{}".format(folder, partijen[partij]['file'])
 
-    # Importeer partij json
+    # Importeer partij JSON
     with open(partij_file, 'r') as fp:
         kandidaten_dict = json.load(fp)
 
@@ -57,3 +92,7 @@ for partij in partijen:
     # Opslaan als JSON
     with open(partij_file, "w") as outfile:
         json.dump(kandidaten_dict, outfile)
+
+
+
+print("✅ Done")
