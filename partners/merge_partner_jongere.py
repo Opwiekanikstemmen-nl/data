45ad241d277cf5b9f51575da9c9c45345ac84ac2
Floris Jansen <florismartijnjansen@gmail.com>
2023-11-21 22:37:46 +0100

    add leeftijd from stemopeenjongere

---
+++ partners/merge_partner_jongere.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import json
+
+parser = argparse.ArgumentParser(
+	description="Merge partnerdata into kandidaten")
+parser.add_argument("-s", "--source", action="store",
+	help="The source JSON")
+
+args = parser.parse_args()
+
+if args.source:
+	source = args.source
+else:
+	print("✋ we need a source file")
+	exit()
+
+with open('../kandidaten.json', 'r') as f:
+	kandidaten = json.load(f)
+
+with open(source, 'r') as f:
+	source = json.load(f)
+
+for key, party in source['kandidaten'].items():
+	for person in party:
+		found = False
+		for kandidaat in kandidaten:
+			if kandidaat['naam'] == person['naam'] and kandidaat['verkiezingen']['tk2023']['partij_naam'] == key:
+				kandidaat['leeftijd'] = person['leeftijd']
+				found = True
+		if not found:
+			print("{0} van {1}".format(person['naam'], key))
+
+with open('../kandidaten.json', 'w', encoding='utf8') as f:
+	json.dump(kandidaten, f, ensure_ascii=False, indent=2)
+
