cf6ac20fc8f15e8cd32c5a1aef1ac184daa07e03
Floris Jansen <florismartijnjansen@gmail.com>
2025-10-16 11:42:19 +0200

    update uni/institute to institution

--- partners/merge_partner_csv.py
+++ partners/merge_partner_csv.py
@@ -32,7 +32,7 @@ with open(source, 'r') as f:
 				kandidaat['vinddebeta'] = True
 				kandidaat['studies'] = [{
 					'study': person[3],
-					'university': person[4]
+					'institute': person[4]
 				}]
 
 				try:
